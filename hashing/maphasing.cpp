447056f39ac2d522b46d640c27168008b063a092
Anushka <anushkasharma156cs@gmail.com>
2026-06-21 10:29:11 +0530

    Learning about hashing
    Studied map hashing implementation.

--- hashing/maphasing.cpp
+++ hashing/maphasing.cpp
@@ -1,25 +1,32 @@
-// map is always store in sorted order, so we can use binary search to find the element in O(log n) time complexity.
-// in dsc order
-#include<map>
-#include<iostream>
+// map is always store in sorted order
+#include <map>
+#include <iostream>
 using namespace std;
-int main(){
+// but write like this it give u time complexity is O(n log n)
+map<int, int> count;  
+int main()
+{
     int n;
-    cin>>n;
-    
+    cin >> n;
+
     int arr[n];
-   for(int i=0;i<n;i++){
-    cin>>arr[i];
-   }
-   map<int ,int> count;
-   for(int i=0;i<n;i++){
-    count[arr[i]]++;
-   }
-   int q;
-   cin>>q;
-   while(q--){
-    int number;
-    cin>>number;
-    cout<<count[number]<<" ";
-   }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        count[arr[i]]++;
+    }
+    
+    //    map<int ,int> count;
+    //    for(int i=0;i<n;i++){
+    //     count[arr[i]]++;
+    //    }
+    
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int number;
+        cin >> number;
+        cout << count[number] << " ";
+    }
 }
